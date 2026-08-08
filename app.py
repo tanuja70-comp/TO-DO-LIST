53eb36513fce323010c51ac3d53993a442ead65b
tanuja70-comp <tanujagangurde70@gmail.com>
2026-08-08 13:50:09 +0530

    added navbar in base.html and add task container in index.html

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+todos = [
+    {"sno":1,"title":"Sample task","desc":"This is a sample task for todo list","status":"Pending"}
+
+]
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
